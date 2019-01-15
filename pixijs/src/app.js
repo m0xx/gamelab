34ca10db414469288fac1bea6622ee2f0cadb56c
maxime <maxime.morin2@gmail.com>
2019-01-15 00:15:41 -0500

    add backgrounds

--- pixijs/src/app.js
+++ pixijs/src/app.js
@@ -23,7 +23,6 @@ function keyboard(value) {
     key.release = undefined;
     //The `downHandler`
     key.downHandler = event => {
-        console.log('Down', event.key)
         if (event.key === key.value) {
             if (key.isUp && key.press) key.press();
             key.isDown = true;
@@ -62,10 +61,13 @@ function keyboard(value) {
     return key;
 }
 
+const HEART_X = 125;
+const LANCE_X = 260;
 class Fighter {
-    constructor(app, textureManager, playerSide) {
+    constructor(app, textureManager, playerSide, onAttack, bgY) {
         this.app = app;
 
+        this.isDead = false;
         this.boundaries = {x: app.screen.width, y: app.screen.height}
         this.textureManager = textureManager;
         this.state = 'IDLE';
@@ -73,7 +75,7 @@ class Fighter {
         this.sprite.animationSpeed = 0.2;
         this.sprite.play();
         this.direction = 1;
-        this.sprite.y = 200;
+        this.sprite.y = bgY;
 
         const isRightPlayer = playerSide === 'right';
 
@@ -89,6 +91,9 @@ class Fighter {
 
         app.ticker.add((delta) => {
             if(this.state === 'ATTACK') {
+                if(this.sprite.currentFrame >= 6) {
+                    onAttack(playerSide, this.sprite)
+                }
                 if(this.sprite.currentFrame === 7) {
                     this.idle();
                 }
@@ -98,11 +103,9 @@ class Fighter {
                     this.dead();
                 }
 
-                this.sprite.y += 5
+                this.sprite.y += 8
             }
             if(this.state === 'RUN') {
-                console.log(this.boundaries, this.sprite.x)
-
                 if(this.direction === 1) {
                     const boundX = isRightPlayer ? this.boundaries.x : this.boundaries.x - this.sprite.width;
                     this.sprite.x = Math.min(this.sprite.x + 5, boundX);
@@ -153,21 +156,25 @@ class Fighter {
         };
     }
     idle() {
+        if(this.isDead) return;
         this.state = 'IDLE';
         this.sprite.textures = this.textureManager.getTextures('IDLE')
         this.sprite.gotoAndPlay(0);
     }
     attack() {
+        if(this.isDead) return;
         this.state = 'ATTACK';
         this.sprite.textures = this.textureManager.getTextures('ATTACK')
         this.sprite.gotoAndPlay(0);
     }
     run() {
+        if(this.isDead) return;
         this.state = 'RUN';
         this.sprite.textures = this.textureManager.getTextures('RUN')
         this.sprite.gotoAndPlay(0);
     }
     die() {
+        this.isDead = true;
         this.state = 'DIE';
         this.sprite.textures = this.textureManager.getTextures('DIE')
         this.sprite.animationSpeed = 0.4;
@@ -200,6 +207,34 @@ class TextureManager {
     }
 }
 
+const backgrounds = [
+    {src: 'assets/bg-1.png', y: 375},
+    {src: 'assets/bg-2.png', y: 375},
+    {src: 'assets/bg-3.png', y: 375},
+    {src: 'assets/bg-4.png', y: 375}
+]
+
+function randomBackground() {
+    const idx = Math.floor(Math.random() * backgrounds.length);
+    return backgrounds[idx];
+}
+
+const titleStyle = new PIXI.TextStyle({
+    fontFamily: 'Arial',
+    fontSize: 48,
+    fontWeight: 'bold',
+    fill: ['#ffffff', '#00ff99'], // gradient
+    stroke: '#4a1850',
+    strokeThickness: 5,
+    dropShadow: true,
+    dropShadowColor: '#000000',
+    dropShadowBlur: 4,
+    dropShadowAngle: Math.PI / 6,
+    dropShadowDistance: 6,
+    wordWrap: true,
+    wordWrapWidth: 440
+});
+
 export default function() {
 
     // The application will create a renderer using WebGL, if possible,
@@ -207,8 +242,16 @@ export default function() {
     // and the root stage PIXI.Container
     const app = new PIXI.Application({width: 1200});
 
+    function playerWin(playerNo) {
+        var text = new PIXI.Text(`Player #${playerNo} wins!`, titleStyle);
+        text.x = app.screen.width / 2 - text.width / 2;
+        text.y = 50;
+
+        app.stage.addChild(text);
+    }
     // load the texture we need
     PIXI.loader
+        .add('bg1', 'assets/bg-1.png')
         .add('assets/1_KNIGHT/knight1-idle.json')
         .add('assets/1_KNIGHT/knight1-attack.json')
         .add('assets/1_KNIGHT/knight1-die.json')
@@ -227,6 +270,17 @@ export default function() {
 
         // app.stage.addChild(idle);
 
+
+            const {src, y: bgY} = randomBackground();
+            var texture = PIXI.Texture.fromImage(src);
+            var background = new PIXI.Sprite(texture);
+            background.x = 0;
+            background.y = 0;
+            background.width = app.screen.width;
+            background.height = app.screen.height;
+            app.stage.addChild(background)
+
+
             const textureManager = new TextureManager();
             textureManager.load('IDLE', 6);
             textureManager.load('ATTACK', 8);
@@ -241,8 +295,37 @@ export default function() {
         // animation.setAnimation('IDLE');
         //
         // app.stage.addChild(sprite);
-        new Fighter(app, textureManager, 'left')
-        new Fighter(app, textureManager, 'right')
+
+        let gameOver = false;
+        const p1 = new Fighter(app, textureManager, 'left', (p) => {
+            if(gameOver) {
+                return;
+            }
+            const reachX = p1.sprite.x + LANCE_X;
+            const heartX = p2.sprite.x - HEART_X;
+            console.log(p1.sprite.x, reachX, heartX)
+            if(reachX >= heartX) {
+                p2.die();
+                gameOver = true;
+                playerWin(1)
+            }
+
+        }, bgY)
+        const p2 = new Fighter(app, textureManager, 'right', (p) => {
+            if(gameOver) {
+                return;
+            }
+            const reachX = p2.sprite.x - LANCE_X;
+            const heartX = p1.sprite.x + HEART_X;
+
+            console.log(p2.sprite.x, reachX, heartX)
+            if(reachX <= heartX) {
+                p1.die();
+                gameOver = true;
+                playerWin(2)
+            }
+        }, bgY)
+
     });
 
     return app;
